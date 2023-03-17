fdb18e9681351f5b05b5304377972ea41eca6fd9
Alex-Torzuk <_alex__08@ukr.net>
2023-03-17 22:44:31 +0200

    +

--- app.js
+++ app.js
@@ -1887,35 +1887,40 @@
 
 // ul.insertAdjacentHTML('beforeend', list.join(''))
 
-//================DOM===================
+//================DOM=================== - Репета
 
 //== Создаем и добавляем коллекцию
 
-const colorPickerOptions = [
-  { label: 'red', color: '#F44336' },
-  { label: 'green', color: '#4CAF50' },
-  { label: 'blue', color: '#2196F3' },
-  { label: 'grey', color: '#607D8B' },
-  { label: 'pink', color: '#E91E63' },
-  { label: 'indigo', color: '#3F51B5' },
-]
-const colorPickerContyainerEl = document.querySelector('.js-color-picker')
+// const colorPickerOptions = [
+//   { label: 'red', color: '#F44336' },
+//   { label: 'green', color: '#4CAF50' },
+//   { label: 'blue', color: '#2196F3' },
+//   { label: 'grey', color: '#607D8B' },
+//   { label: 'pink', color: '#E91E63' },
+//   { label: 'indigo', color: '#3F51B5' },
+// ]
+// const colorPickerContyainerEl = document.querySelector('.js-color-picker')
 
-const elements = colorPickerOptions.map(option => {
-  const buttonEl = document.createElement('button');
-  buttonEl.type = 'button';
-  buttonEl.textContent = option.label;
-  buttonEl.style.backgroundColor = option.color;
-  return buttonEl
-})
-//const option = colorPickerOptions[1]
-
-
+// const elements = colorPickerOptions.map(option => {
+//   const buttonEl = document.createElement('button');
+//   buttonEl.type = 'button';
+//   buttonEl.textContent = option.label;
+//   buttonEl.style.backgroundColor = option.color;
+//   return buttonEl
+// })
+// //const option = colorPickerOptions[1]
 
 
-console.log(elements)
 
-colorPickerContyainerEl.append(...elements)
+
+// console.log(elements)
+
+// colorPickerContyainerEl.append(...elements)
+
+//================DOM=================== - Павел
+
+const uncle = document.querySelector('li#uncle')
+console.log(uncle.previousElementSibling.firstElementChild.firstElementChild)
 
 
 
