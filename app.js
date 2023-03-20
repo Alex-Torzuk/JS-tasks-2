9e832cc8c5842c48cd1fa2503d2a489e54287e47
Alex-Torzuk <_alex__08@ukr.net>
2023-03-21 00:33:03 +0200

    +

--- app.js
+++ app.js
@@ -1929,8 +1929,46 @@
 // console.dir(aunt.classList.add('tomato'))
 
 // const myDiv = document.createElement('div')
-// myDiv.innerHTML = '<h1>I am a new div </h1>' 
+// myDiv.innerHTML = '<h1>I am a new div </h1>'
 // document.body.appendChild(myDiv)
+
+//---Навигация по DOM--------
+
+console.log(document);
+
+const body = document.body;
+console.log(body);
+
+const list = body.firstElementChild;
+console.log(list);
+
+const firstListItem = list.firstElementChild;
+console.log(firstListItem);
+
+const listItems = list.children;
+console.log(listItems);
+
+//---Поиск элементов--------
+
+const listWithId = document.querySelector('#menu');
+listWithId.style.textTransform = 'uppercase';
+listWithId.style.fontSize = '24px';
+console.log(listWithId);
+
+const listWithClass = document.querySelector('.menu');
+console.log(listWithClass);
+
+const menuItemsByTagName = document.querySelectorAll("li");
+console.log(menuItemsByTagName);
+
+const menuItemsByClass = document.querySelectorAll(".menu-item");
+console.log(menuItemsByClass);
+
+const firstMenuItem = document.querySelector(".menu-item");
+firstMenuItem.style.color = 'tomato';
+console.log(firstMenuItem);
+
+
 
 
 
